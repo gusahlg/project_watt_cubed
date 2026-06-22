c61db917d8a5df68e57d640a57e272ced6df42bd
gusahlg <gusahlg@gmail.com>
2026-06-22 20:08:14 +0200

    Added in movement

---
+++ src/keys/mod.rs
@@ -0,0 +1,2 @@
+/// This file handles all the key clicking and exposes functions that send back results to the caller
+pub mod movement;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 /// This is some prototype code for the game, the api it uses for interacting with Vulkan is under
 /// development so things will change on this end as well to reflect those changes.
 use raylib::prelude::*;
+
+mod keys;
+use keys::*;
+
+use self::keys::movement::move_player_camera;
 fn main() {
     let (mut rl, thread) = raylib::init()
         .size(1280, 720)
@@ -9,7 +14,7 @@ fn main() {
 
     rl.set_target_fps(100);
 
-    let camera = Camera3D::perspective(
+    let mut camera = Camera3D::perspective(
         Vector3::new(10.0, 10.0, 10.0),
         Vector3::new(0.0, 0.0, 0.0),
         Vector3::new(0.0, 1.0, 0.0),
@@ -17,6 +22,8 @@ fn main() {
     );
 
     while !rl.window_should_close() {
+        let direction = movement::get_move_direction(&rl);
+        move_player_camera(&mut camera, direction);
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::SKYBLUE);
 
